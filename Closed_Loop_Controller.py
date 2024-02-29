347866b5d1aecec5fedaa072f9ce19bd40815492
alialauren1 <157066441+alialauren1@users.noreply.github.com>
2024-02-28 19:01:09 -0800

    updates

--- Closed_Loop_Controller.py
+++ Closed_Loop_Controller.py
@@ -13,14 +13,14 @@ class Controller:
 
     def run(self, measured_output):
          #actuation value is Kp*(theta_set - enc2.read() value
-         self.measured_output = measured_output
-         err = self.set_point - self.measured_output
+         #self.measured_output = measured_output
+         err = self.set_point - measured_output
          actuation = self.kp*err
          current_time = utime.ticks_ms()
          #if not self.time_value.full():
          self.time_passed = utime.ticks_diff(current_time, self.start)
          self.time_value.put(self.time_passed)
-         self.position.put(self.measured_output)
+         self.position.put(measured_output)
          #utime.sleep_ms(10) # does this go in main or control?
          return actuation
         
